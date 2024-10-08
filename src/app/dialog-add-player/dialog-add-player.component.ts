660b3164a5ea4c9ff85b1a62fab67238f1985338
Sascha <saschaborowski@outlook.de>
2024-10-08 09:16:15 +0200

    first playable version

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogClose, MatDialogTitle } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dialog-add-player',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatDialogModule, MatFormFieldModule, MatInputModule, FormsModule],
+  templateUrl: './dialog-add-player.component.html',
+  styleUrl: './dialog-add-player.component.scss'
+})
+export class DialogAddPlayerComponent {
+  constructor(public dialog: MatDialog) { }
+  name: string = '';
+
+  onNoClick() {
+  }
+}
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
+import { PlayerComponent } from "../player/player.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { GameInfoComponent } from "../game-info/game-info.component";
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, GameInfoComponent],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+
+export class GameComponent {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+
+
+      const card = this.game.stack.pop();
+      if (card !== undefined) {
+        this.currentCard = card;
+        console.log(this.currentCard);
+        this.pickCardAnimation = true;
+        console.log('New card:' + this.currentCard);
+        console.log('Game is:', this.game);
+
+        this.game.currentPlayer++;
+        this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
+        setTimeout(() => {
+          this.game.playedCards.push(this.currentCard);
+          this.pickCardAnimation = false;
+        }, 1000);
+      }
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name:string) => {
+      console.log('The dialog was closed', name);
+      this.game.players.push(name);
+      if (name !== undefined) {
+      }
+    });
+  }
+}
